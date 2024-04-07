56fd219c7510bb32a0d86b95c225dde213da9bfb
Noel O'SULLIVAN <noel.oo.sullivan@gmail.com>
2024-04-07 20:57:52 +0200

    Screen updates with device change in tools. Just nice to have

--- src/app/components/layout.component.ts
+++ src/app/components/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MenuComponent } from './menu/menu/menu.component';
 import { ContentComponent } from './content/content/content.component';
 import { ArrowComponent } from '../shared/components/arrow/arrow.component';
@@ -18,8 +18,27 @@ export class LayoutComponent {
 
   @HostListener('window:resize', ['$event'])
   onWindowResize() {
+    
+    console.log("window.innerWidth", window.innerWidth);
+    console.log("window.innerHeight", window.innerHeight);
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
+    if(this.screenWidth != this.portraitWidth || this.screenHeight != this.portraitHeight) {
+      if (this.screenWidth < this.screenHeight) {
+        this.portraitWidth = this.screenWidth;
+        this.portraitHeight = this.screenHeight;
+        this.landscapeWidth = this.screenHeight;
+        this.landscapeHeight = this.screenWidth;
+      }
+    } else {
+      if (this.screenWidth > this.screenHeight) {
+        this.portraitWidth = this.screenHeight;
+        this.portraitHeight = this.screenWidth;
+        this.landscapeWidth = this.screenWidth;
+        this.landscapeHeight = this.screenHeight;
+      }
+    }
+    
     this.updateLayout();
   }
 
@@ -28,38 +47,74 @@ export class LayoutComponent {
   screenWidth!: number;
   screenHeight!: number;
 
+  portraitWidth!: number;
+  portraitHeight!: number;
+  landscapeWidth!: number;
+  landscapeHeight!: number;
+
+
   ngAfterViewInit(): void {
+    console.log("-- window.innerWidth", window.innerWidth);
+    console.log("-- window.innerHeight", window.innerHeight);
 
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
-
-    if(this.screenWidth < this.screenHeight) {
-      this.contentLayout.nativeElement.style["height"] = this.screenHeight - 180 + 'px';
-      this.contentLayout.nativeElement.style["width"] = '100%';
-      this.contentLayout.nativeElement.style["top"] = '0';
-      this.contentLayout.nativeElement.style["left"] = '0';
-      // this.contentLayout.nativeElement.style["background"] = 'yellow';
-  
-      this.menuLayout.nativeElement.style["height"] = '180px';
-      this.menuLayout.nativeElement.style["width"] = '100%';
-      this.menuLayout.nativeElement.style["bottom"] = '0';
-      this.menuLayout.nativeElement.style["left"] = '0';
-      // this.menuLayout.nativeElement.style["background"] = 'green';
+    if (this.screenWidth < this.screenHeight) {
+      this.portraitWidth = this.screenWidth;
+      this.portraitHeight = this.screenHeight;
+      this.landscapeWidth = this.screenHeight;
+      this.landscapeHeight = this.screenWidth;
     } else {
-      this.contentLayout.nativeElement.style["width"] = this.screenWidth / 4 * 3 + 'px';
-      this.contentLayout.nativeElement.style["height"] = '100%';
-      this.contentLayout.nativeElement.style["top"] = '0';
-      this.contentLayout.nativeElement.style["left"] = '0';
-  
-      this.menuLayout.nativeElement.style["width"] = this.screenHeight / 4 + 'px';
-      this.menuLayout.nativeElement.style["height"] = '100%';
-      this.menuLayout.nativeElement.style["top"] = '0';
-      this.menuLayout.nativeElement.style["right"] = '0';
+      this.portraitWidth = this.screenHeight;
+      this.portraitHeight = this.screenWidth;
+      this.landscapeWidth = this.screenWidth;
+      this.landscapeHeight = this.screenHeight;
     }
+    console.log("this.portraitWidth", this.portraitWidth);
+    console.log("this.portraitHeight", this.portraitHeight);
+    console.log("this.landscapeWidth", this.landscapeWidth);
+    console.log("this.landscapeHeight", this.landscapeHeight);
+    this.updateLayout();
+
   }
 
   updateLayout() {
-    this.contentLayout.nativeElement.style["height"] = this.screenHeight - 180 + 'px';
-    this.menuLayout.nativeElement.style["height"] = '180px';
+    // this.contentLayout.nativeElement.style["height"] = this.screenHeight - 180 + 'px';
+    // this.menuLayout.nativeElement.style["height"] = '180px';
+    if (this.screenWidth < this.screenHeight) {
+      console.log("PORTRAIT");
+      // this.contentLayout.nativeElement.style["height"] = this.screenHeight - 180 + 'px';
+      // debugger;
+      this.contentLayout.nativeElement.style["height"] = this.portraitHeight - 180 + 'px';
+      // this.contentLayout.nativeElement.style["width"] = '100%';
+      this.contentLayout.nativeElement.style["width"] = this.portraitWidth + 'px';
+      this.contentLayout.nativeElement.style["top"] = '0';
+      this.contentLayout.nativeElement.style["left"] = '0';
+      this.contentLayout.nativeElement.style["background"] = 'none';
+
+      this.menuLayout.nativeElement.style["height"] = '180px';
+      this.menuLayout.nativeElement.style["width"] = this.portraitWidth + 'px';
+      this.menuLayout.nativeElement.style["bottom"] = '0';
+      this.menuLayout.nativeElement.style["top"] = 'auto';
+      this.menuLayout.nativeElement.style["left"] = '0';
+      this.menuLayout.nativeElement.style["right"] = 'auto';
+      this.menuLayout.nativeElement.style["background"] = 'none';
+    } else {
+      console.log("LANDSCAPE");
+      // debugger;
+      this.contentLayout.nativeElement.style["width"] = this.landscapeWidth - 180 + 'px';
+      this.contentLayout.nativeElement.style["height"] = this.landscapeHeight + 'px';
+      this.contentLayout.nativeElement.style["top"] = '0';
+      this.contentLayout.nativeElement.style["left"] = '0';
+      this.contentLayout.nativeElement.style["background"] = 'yellow';
+
+      this.menuLayout.nativeElement.style["width"] = '180px';
+      this.menuLayout.nativeElement.style["height"] = this.landscapeHeight + 'px';
+      this.menuLayout.nativeElement.style["top"] = '0';
+      this.menuLayout.nativeElement.style["bottom"] = 'auto';
+      this.menuLayout.nativeElement.style["right"] = '0';
+      this.menuLayout.nativeElement.style["left"] = 'auto';
+      this.menuLayout.nativeElement.style["background"] = 'green';
+    }
   }
 }
